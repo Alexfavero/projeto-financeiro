d8ac2f26a67d4d7f49ecd5226b15f73c1cefa2d3
alexfavero4036@gmail.com <alexfavero4036@gmail.com>
2026-02-25 10:44:30 -0300

    Configurações do arquivo de conexão com banco de dados

---
+++ Backend/Financeiro.Api/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Financeiro.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financeiro.Api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<ContaAReceber> ContasAReceber { get; set; }
+        public DbSet<ContaAPagar> ContasAPagar { get; set; }
+        public DbSet<Parcela> Parcelas { get; set; }
+
+    }
+}
